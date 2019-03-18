cf6e348215b4fb0051381dc41f3f5b36a8c43189
Kirill <kirilkin.97@gmail.com>
2019-03-18 21:28:29 +0300

    Create infrastructure to work with .json book catalogue

---
+++ Prikhodko/BookCatalogue/JsonBookRepository.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace BookCatalogue
+{
+    public class JsonBookRepository : IBookRepository
+    {
+        private static readonly string cataloguePath = "books.json";
+        private bool booksLoaded = false;
+        private List<Book> books;
+        public List<Book> Books
+        {
+            get
+            {
+                if (!booksLoaded)
+                {
+                    using (var context = new JsonDataContext<Book>(cataloguePath))
+                    {
+                        books = context.LoadData().ToList();
+                    }
+                    booksLoaded = true;
+                }
+                return books;
+            }
+        }
+
+        public JsonBookRepository()
+        {
+            books = new List<Book>();
+        }
+
+        
+        public void Add(Book book)
+        {
+            if (book == null)
+            {
+                return;
+            }
+            else
+            {
+                book.Id = GetId();
+                using (var context = new JsonDataContext<Book>(cataloguePath))
+                {
+                    books.Add(book);
+                    context.Save(books);
+                }
+            }
+        }
+
+        private int GetId()
+        {
+            return Books.Count + 1;
+        }
+
+        public Book GetBook(int id)
+        {
+            if(id <= 0)
+            {
+                return null;
+            }
+            else
+            {
+                return books.FirstOrDefault(b => b.Id == id);
+            }
+        }
+
+        public List<Book> GetBooks()
+        {
+            return Books;
+        }
+
+        public void Remove(int id)
+        {
+            if (id <= 0)
+            {
+                return;
+            }
+            else
+            {
+                using (var context = new JsonDataContext<Book>(cataloguePath))
+                {
+                    books.Remove(Books.FirstOrDefault(b => b.Id == id));
+                    context.Save(books);
+                }
+            }
+        }
+
+        public void Update(Book book)
+        {
+            if(book == null || book.Id <= 0)
+            {
+                return;
+            }
+            else
+            {
+                using (var context = new JsonDataContext<Book>(cataloguePath))
+                {
+                    Book temp = books.FirstOrDefault(b => b.Id == book.Id);
+                    temp.Author = book.Author;
+                    temp.Name = book.Name;
+                    temp.YearOfIssue = book.YearOfIssue;
+                    context.Save(books);
+                }
+            }
+        }
+    }
+}
---
+++ Prikhodko/BookCatalogueTests/JsonDataContextTests.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BookCatalogue;
+using System.IO;
+using System.Collections.Generic;
+
+namespace BookCatalogueTests
+{
+    [TestClass]
+    public class JsonDataContextTests
+    {
+        [TestMethod]
+        public void Ctor_NoParameters_ShouldReturnObject()
+        {
+            var dataContext = new JsonDataContextTests();
+            Assert.IsNotNull(dataContext);
+        }
+
+        [TestMethod]
+        public void LoadData_DataExists_ShouldReturnCollection()
+        {
+            //Arrange
+            var dataContext = new JsonDataContext<Book>("books.json");
+            var books = new List<Book>();
+            var book = new Book();
+            book.Id = 1;
+            book.Name = "Hello";
+            book.Author = "World";
+            book.YearOfIssue = 1984;
+            books.Add(book);
+            dataContext.Save(books);
+
+            //Act
+            IEnumerable<Book> actual = dataContext.LoadData();
+
+            //Assert
+            Assert.IsNotNull(actual);
+        }
+
+        [TestMethod]
+        public void Save_EmptyList_ShouldDeleteFile()
+        {
+            //Arrange
+            var dataContext = new JsonDataContext<Book>("books.json");
+            if(!File.Exists("books.json"))
+            {
+                File.Create("books.json");
+            }
+
+            //Act
+            dataContext.Save(null);
+
+            //Assert
+            bool fileExists = File.Exists("books.json");
+            Assert.IsFalse(fileExists);
+        }
+    }
+}
