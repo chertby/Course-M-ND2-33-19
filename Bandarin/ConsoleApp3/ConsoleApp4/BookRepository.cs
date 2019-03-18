4d2e8411fc5d23323f2f37e9929e4bf36183c1cc
bandaryn <a.bandaryn@gmail.com>
2019-03-19 00:42:33 +0300

    Add Lab1

---
+++ Bandarin/ConsoleApp3/ConsoleApp4/BookRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    public class BookRepository: IRepository<Book>
+    {
+        private readonly List<Book> bookList;
+        private IFileWorker obb1;
+
+        public BookRepository(IFileWorker obb)
+        {
+            obb1 = obb;
+            bookList = obb1.ReadFromJsonFile<List<Book>>("output1.json");
+        }
+
+        public Book Get(int id)
+        {
+            var bookGot = bookList.FirstOrDefault(x => x.ID == id);
+            if (bookGot != null)
+            {
+                return bookGot;
+            }
+
+            throw new Exception("Book with such id not found");
+        }
+        public void Add(Book book)
+        {
+            bookList.Add(book);
+        }
+        public void Edit(Book book)
+        {
+            Remove(book.ID);
+            Add(book);
+        }
+        public void Remove(int id)
+        {
+            Book forDel = Get(id);
+            bookList.Remove(forDel);
+        }
+        public void Save()
+        {
+           obb1.WriteToJsonFile<List<Book>>("output1.json", bookList);
+        }
+    }
+}
---
+++ Bandarin/ConsoleApp3/UnitTestProject2/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject2")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3b18372d-67ac-41cf-a5f5-df6fe15a43a5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
