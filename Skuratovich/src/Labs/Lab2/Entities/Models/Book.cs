d9667e3c6020674bd08627ce43f90847acde0a63
askuratovich <a.skuratovich@gmail.com>
2019-03-25 01:48:27 +0300

    Add view

--- Skuratovich/src/Labs/Lab2/Entities/Models/Book.cs
+++ Skuratovich/src/Labs/Lab2/Entities/Models/Book.cs
@@ -1,8 +1,13 @@
-﻿namespace Lab2.Entities.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lab2.Entities.Models
 {
     public class Book : IEntity
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Title is required")]
+        [StringLength(60, ErrorMessage = "Title can't be longer than 60 characters")]
         public string Title { get; set; }
         //public string Description { get; set; }
         //public string Author { get; set; }
--- Skuratovich/src/Labs/Lab2/Lab2.Contracts/IBookRepository.cs
+++ Skuratovich/src/Labs/Lab2/Lab2.Contracts/IBookRepository.cs
@@ -6,7 +6,7 @@ namespace Lab2.Contracts
     public interface IBookRepository : IRepositoryBase<Book>
     {
         IEnumerable<Book> GetAllBooks();
-        Book GetBookById(int id);
+        Book GetBookById(int? id);
         void CreateBook(Book book);
         void UpdateBook(Book book);
         void DeleteBook(Book book);
