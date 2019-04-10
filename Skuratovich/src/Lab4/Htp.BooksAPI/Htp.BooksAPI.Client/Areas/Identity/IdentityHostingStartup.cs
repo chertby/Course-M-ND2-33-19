f5f1a9513c94c23e4c7eedd7f4156b067d66ccda
askuratovich <a.skuratovich@gmail.com>
2019-04-10 14:45:28 +0300

    Scaffold identity
    Scaffold identity into a Razor project with authorization

---
+++ Skuratovich/src/Lab4/Htp.BooksAPI/Htp.BooksAPI.Client/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+using System;
+using Htp.BooksAPI.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Htp.BooksAPI.Client.Areas.Identity.IdentityHostingStartup))]
+namespace Htp.BooksAPI.Client.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<ApplicationDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("ApplicationDbContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>()
+                    .AddEntityFrameworkStores<ApplicationDbContext>();
+            });
+        }
+    }
+}
