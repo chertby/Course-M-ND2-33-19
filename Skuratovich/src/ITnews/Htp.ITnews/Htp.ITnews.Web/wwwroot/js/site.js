b47beffe56f9bf909cdc65e8b392a4844c609a44
askuratovich <a.skuratovich@gmail.com>
2019-06-06 19:39:04 +0300

    Add theme

--- Skuratovich/src/ITnews/Htp.ITnews/Htp.ITnews.Web/wwwroot/js/site.js
+++ Skuratovich/src/ITnews/Htp.ITnews/Htp.ITnews.Web/wwwroot/js/site.js
@@ -2,3 +2,8 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+$('.theme').on('click', function () {
+    Cookies.set('<ITnews>.Theme', $(this).data('theme'), { expires: 1 });
+    location.reload();
+});
